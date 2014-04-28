3f29ce1e3ccc2efdab3ea7df4d5cae42f524fbd3
Daniel Abraham <abrahamd@akamai.com>
2014-04-28 00:45:58 -0400

    moved custom to after themes

--- custom/prompts.zsh
+++ custom/prompts.zsh
@@ -1,2 +1,3 @@
-PROMPT=$'%n@%m %# '
+PROMPT='${ret_status}%{$fg_bold[green]%}%n@%m %{$fg_bold[blue]%}$(git_prompt_info)%{$fg_bold[blue]%} % %{$reset_color%}'
+
 RPROMPT='%{$fg[cyan]%}%t %{$fg[cyan]%}%~ %{$reset_color%}'
