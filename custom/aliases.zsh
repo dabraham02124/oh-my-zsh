cf7d0e506fcef6d61f8c7863bf38889eba7c35b5
Daniel Abraham <abrahamd@akamai.com>
2014-04-29 07:53:27 -0400

    moved aliases into oh-my-zsh file,rather than sourced by it

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -1,1 +1,23 @@
-source ~/.aliases
+ctail() {
+tail -n 40 -f $1 | awk '
+  BEGIN    {color="30m" }
+  /INFO/   {color="32m"}
+  /WARN/   {color="33m"}
+  /ERROR/  {color="31m"}
+  /SEVERE/ {color="31m"}
+  /FATAL/  {color="31m"}
+           {print "\033["color $0" \033[39m"}
+'
+}
+
+
+alias e=emacsclient
+alias emacs=/Applications/Emacs.app/Contents/MacOS/Emacs
+alias ea='emacsclient ~/.aliases'
+alias ez='emacsclient ~/.zshrc'
+
+alias la='ls -laF'
+alias ll='ls -lF'
+
+alias sa='source ~/.aliases'
+alias sz='source ~/.zshrc'
